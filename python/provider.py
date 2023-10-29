4c9118e7b0b53b833c69fe37fe56841eceee33b5
cp-tech-lab <lab.cp.tech@gmail.com>
2023-10-29 17:29:42 +0100

    removed mocked process

--- python/provider.py
+++ python/provider.py
@@ -1,6 +1,5 @@
 from multiprocessing import Queue
 from jobs import AcquisitionJob, DecoderJob, CoreJob
-from test.mockedJob import MockedJob
 from json import load
 
 config = load(open('config.json'))
@@ -12,10 +11,6 @@ _acquisition_task = AcquisitionJob()
 _decoder_task = DecoderJob()
 _core_task = CoreJob(config.get('UnityAppHost'), config.get('UnityAppPort'))
 
-
-_mocked_task = MockedJob(_rq)
-
-
 def acquire(rq: Queue, port: str, fs: float):
     _acquisition_task.run(rq, port, fs)
 
@@ -24,6 +19,3 @@ def decode(rq: Queue, wq: Queue):
 
 def core(q: Queue, mapping: str, fu: float, fs: float):
     _core_task.run(q, mapping, fu, fs)
-
-def mock_random_data(rq: Queue):
-    _mocked_task.run(rq=rq)
